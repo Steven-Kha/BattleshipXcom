460d71cbc22d764473f5bb45c35dc7d0d2853387
wbui3 <buiquangwilliam@yahoo.com>
2018-02-11 17:16:36 -0800

    added the checkRange function

--- Source.cpp
+++ Source.cpp
@@ -116,92 +116,85 @@ void moveOne(int & oneX, int & oneY, int & twoX, int & twoY, ship player, ship p
 	} while ((xNum == 7) || (coordY < 1 || coordY > 6) || (!rangePass));
 
 	if (board[coordY - 1][xNum] != board[twoY][twoX]) {
-				board[coordY - 1][xNum] = player.getRep();
-				board[oneY][oneX] = 'X';
-				oneY = coordY - 1;
-				oneX = xNum;
-				cout << "Coordinates received!... \n";
-				system("pause");
-				system("cls");
-				print(player, player2, board);
-				//cout << "success! \n";
-				//to save time compiling when it succeeds
-				/*oneY = 3;
-				oneX = 3;
-				board[oneY][oneX] = '$';
-				board[coordY - 1][xNum] = 'X';
-				moveOne(oneX, oneY, twoX, twoY, board);*/
-			}
+		board[coordY - 1][xNum] = player.getRep();
+		board[oneY][oneX] = 'X';
+		oneY = coordY - 1;
+		oneX = xNum;
+		cout << "Coordinates received!... \n";
+		system("pause");
+		system("cls");
+		print(player, player2, board);
+	}
 
-			else {
-				cout << "That's where the other ship is!";
-				system("pause");
-				moveOne(oneX, oneY, twoX, twoY, player, player2, board);
-			}
+	else {
+		cout << "That's where the other ship is!";
+		system("pause");
+		moveOne(oneX, oneY, twoX, twoY, player, player2, board);
+	}
 
 
 }
 
-bool checkRange(int range, int xNum, int oneX, int oneY, int twoX, int twoY,
-	int coordY, ship player, ship player2, char board[][6]){
+bool checkRange(int range, int xDest, int xCurr, int yCurr, int twoX, int twoY,
+	int yDest, ship player, ship player2, char board[][6]) {
 
 	bool xPass = false;
 	bool yPass = false;
 
-	// if ((xNum < 0 || xNum > 5)) {
-	// 	cout << "Please re-enter valid X: ";
-	// 	system("pause");
-	// 	system("cls");
-	// 	print(player, player2, board);
-	// 	moveOne(oneX, oneY, twoX, twoY, player, player2, board);
-	// 	return false;
-	// }
-
-	if ((xNum - oneX > range || xNum - oneX < 0)) // xNum is destination; oneX is current location
+	// Here, we prove (Destination - current location)
+	// This is a proof by contradiction
+	//ex: dest :5 current location: 3 range: 2
+	//5-3 = 2 and 2>0
+	// but what if dest: 1 cur: 5
+	// 1 - 5 = -4 so it would pass
+	// so check if destination is greater or equal to current location
+	if ((xDest - xCurr <= range && xDest >= xCurr)) // xDest is destination; xCurr is current location
 	{
-		// if the ship is moving beyond the speed (range) in a positive direction,
-		// OR the destination is negative, then xPass is false;
+		xPass = true;
 	}
-	else { xPass = true; }
 
 	if (xPass == false) { //
 
-						  // since xPass is false, we check the speed with (current location - destination)
-						  // check the displacement from current location and destiation is above the speed value
-						  // if it is below the speed value, double check if destination is greater than current location
-						  // ex: 3 - 6 > 2 is false but 3- 6 > 0 is true so player must re-enter valid x
-						  // if destination was greater than current location, it's because the destination was above the speed value
-						  // from the current location
-						  //
-		if (oneX - xNum > range || oneX - xNum < 0) { //player.getSpeed() is range
+		//but what if:
+		// dest: 3 curr: 5
+		// 3-5 = -2 but
+		//5-3 =2 so it should pass
+		//so check current - destination is = or below range but current is greater than destination
+		//
+		if (xCurr - xDest <= range && xCurr > xDest) {
+			xPass = true;
+		}
+		else {
 			cout << "Please re-enter valid X: ";
 			system("pause");
 			system("cls");
 			print(player, player2, board);
 			return false;
 		}
+
 	}
-
-
 
 	//------------------------------check y coordinates now
 	//check if coordinate is out of bounds and speed displacement is greater than one or
 	//less than zero
 	/*cout << "Enter Y coordinates: ";
-	cin >> coordY;*/
+	cin >> yDest;*/
 
 	//destination - current location
 	//we are offsetting the coord y because the board starts at one instead of zero
-	if ((coordY - 1) - oneY > player.getSpeed() || (coordY - 1) - oneY < 0) {
+	if ((yDest - 1) - yCurr <= range && (yDest - 1) >  0) {
 		//cout << "Failed Y part 1 \n";
+		yPass = true;
 	}
-	else { yPass = true; }
 
 	if (yPass == false) {
 		//current location - destination
 		//we are offsetting the coord y because the board starts at one instead of zero
-		if (((oneY + 1) - coordY > player.getSpeed()) || ((oneY + 1) - coordY < 0)) {
+		if (  ( (yCurr + 1) - yDest <= range  ) && ( (yCurr + 1) > yDest)   ) {
 			//cout << "Failed Y part 2 \n";
+			yPass = true;
+		}
+		else {
 			cout << "Please re-enter valid Y: ";
 			system("pause");
 			system("cls");
@@ -211,7 +204,7 @@ bool checkRange(int range, int xNum, int oneX, int oneY, int twoX, int twoY,
 		//cout << "I passed part 2! \n";
 
 	}
-	if (board[coordY - 1][xNum] == board[oneY][oneX]) {
+	if (board[yDest - 1][xDest] == board[yCurr][xCurr]) {
 		char answer;
 		cout << "You didn't move. \n"
 			<< "Re-enter coordinates or quit? \n";
@@ -239,14 +232,14 @@ void attack(int oneX, int oneY, int & twoX, int & twoY, ship & player, ship & pl
 	int coordY = 0;
 	int xNum = 0;
 	bool rangePass = false;
-	
+
 	do
 	{
 		cout << "Loading rounds...\n";
 		cout << "Enter X coordinates: ";
 		cin >> coordX;			// coordX is user input
 
-									//converting Letters to x coordinates
+								//converting Letters to x coordinates
 		if (coordX == 'A' || coordX == 'a') { xNum = 0; }
 		else if (coordX == 'B' || coordX == 'b') { xNum = 1; }
 		else if (coordX == 'C' || coordX == 'c') { xNum = 2; }
